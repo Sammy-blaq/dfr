8de3f34d757a11c6df02b9cf78faf75b616de97f
Abdulsamad <aya2100735.com@buk.edu.ng>
2026-06-19 20:45:59 +0100

    Injest data with Django rest framework view

--- py_client/basic.py
+++ py_client/basic.py
@@ -4,7 +4,7 @@ endpoint = "https://httpbin.org/status/200/"
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, params={"product_id": 123}, json={"query": "hello world"})
+get_response = requests.post(endpoint, json={"title": "Hello world", 'price': 89})
 print(get_response.json())
 # print(get_response.text)
 # print(get_response.status_code)
