787bb7278b21758806f4959af5752eb32a848c95
Abdulsamad <aya2100735.com@buk.edu.ng>
2026-06-21 12:51:14 +0100

    Token Authorization

--- py_client/create.py
+++ py_client/create.py
@@ -6,5 +6,9 @@ data = {
     'price': 134
 }
 
-get_response = requests.post(endpoint, json=data)
+headers = {
+    "Authorization": "Bearer 1df4844cac8549c1d589b0a83e3bea9b479bd271"
+}
+
+get_response = requests.post(endpoint, json=data, headers=headers)
 print(get_response.json())
