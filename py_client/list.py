78ed91f4b16ee816f983b9c2aaf053bb5731b351
Abdulsamad <aya2100735.com@buk.edu.ng>
2026-07-30 21:18:57 +0100

    Pagination

--- py_client/list.py
+++ py_client/list.py
@@ -18,4 +18,12 @@ if auth_response.status_code == 200:
 
 
     get_response = requests.get(endpoint, headers=headers)
-    print(get_response.json())
+    print()
+    data = get_response.json()
+    next_url = data['next']
+    results = data['results']
+    print(next_url)
+    print(results)
+
+    # if next_url is not None:
+    #     get_response = requests.get(next_url, headers=headers)
